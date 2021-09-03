60709dd06770ffca0e952ab571ffb9e8c58ca030
md.rubel <md.rubel@HOAP2406101764A>
2021-09-04 00:21:35 +0600

    Add Controller and View

--- TaskList.Web/Controllers/HomeController.cs
+++ TaskList.Web/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using TaskList.DAL;
 using TaskList.Web.Models;
 
 namespace TaskList.Web.Controllers
@@ -12,15 +13,21 @@ namespace TaskList.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private TaskListDbContext _db;
+        public HomeController(ILogger<HomeController> logger, TaskListDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Tasks()
+        {
+            return View(_db.TaskItems.AsEnumerable());
         }
 
         public IActionResult Privacy()
--- TaskList.Web/Startup.cs
+++ TaskList.Web/Startup.cs
@@ -11,6 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TaskList.DAL;
+using TaskList.Services.EmailService;
 
 namespace TaskList.Web
 {
